f9acb482e76a9e812f96c8207809046b16256b3b
HG\B1208141 <tecforce@hwa-guan.com.tw>
2023-12-08 08:20:29 +0800

    清除重置

---
+++ HG_Subscribe/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using HG_Subscribe.Models;
+using HGCryptor;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace HG_Subscribe.Controllers
+{
+    public class AdminController : Controller
+    {
+        private static Cryptor cryptor = new Cryptor();
+        private static Models.ClikGoEntities db = new Models.ClikGoEntities();
+
+        // GET: Admin
+        [HttpPost]
+        public string login(string account, string password, string token)
+        {
+            Cryptor.apiResultObj RC = cryptor.verifyAPISecret(token);
+            if (!RC.result) return JsonConvert.SerializeObject(RC);
+
+            string acc = cryptor.encryptData(account);
+            string pwd = cryptor.encryptData(password);
+            Cryptor.apiResultObj result = new Cryptor.apiResultObj();
+
+            List<administrator> admin = db.administrator.Where(a => a.admAccount == acc && a.admPassword == pwd).ToList();
+
+            if (admin.Count > 0)
+            {
+                result.result = true;
+                result.code = 200;
+                result.message = admin;
+            }
+
+            //string resultStr = JsonConvert.SerializeObject(result);
+            //resultStr = ((resultStr.Replace("\\\"", "\"")).Replace("\"[", "[")).Replace("]\"", "]");
+
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+}
