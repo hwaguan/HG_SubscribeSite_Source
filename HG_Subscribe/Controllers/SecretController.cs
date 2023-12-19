d90ce0df1a10c4d62195448a0b23fde85b1e1945
HG\B1208141 <tecforce@hwa-guan.com.tw>
2023-12-19 17:27:50 +0800

    rebuild

---
+++ HG_Subscribe/Controllers/SecretController.cs
@@ -0,0 +1,20 @@
+﻿using HGCryptor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace HG_Subscribe.Controllers
+{
+    public class SecretController : Controller
+    {
+        private static Cryptor cryptor = new Cryptor();
+        [HttpPost]
+        public string getTransferToken()
+        {
+            return cryptor.getAPISecret();
+        }
+    }
+}
