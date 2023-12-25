926631d61f2dc4d77b1187a0034ffe55834c5245
HG\B1208141 <tecforce@hwa-guan.com.tw>
2023-12-25 11:00:37 +0800

    fix data error

--- HG_Subscribe/Controllers/AdminController.cs
+++ HG_Subscribe/Controllers/AdminController.cs
@@ -228,7 +228,7 @@ namespace HG_Subscribe.Controllers
                     {
                         SB.Append(subMenuItems > 0 ? "," : "");
                         SB.Append("{");
-                        SB.Append("'Action':'" + sMenu.menuID + "'");
+                        SB.Append("'ID':'" + sMenu.menuID + "'");
                         SB.Append(",'Action':'" + sMenu.menuName + "'");
                         SB.Append(",'Text':'" + sMenu.menuText + "'");
                         SB.Append("}");
